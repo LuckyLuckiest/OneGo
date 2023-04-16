212ce248099e4bde342fa0a28b0270541a8dd58f
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-04-16 20:02:23 +0400

    working with obstacles

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,5 @@
 const player = document.getElementById("player");
+var started = false;
 
 function move() {
     if (player.class == "jump") return;
@@ -10,12 +11,21 @@ function jump() {
     player.classList.remove("move");
     player.classList.add("jump");
 
-    setTimeout(function() {
+    setTimeout(function () {
         player.classList.remove("jump");
         move();
-    }, 300);
+    }, 500);
 }
 
-document.addEventListener("keydown", function(event) {
-    jump();
+function obsMove() {
+    const obs = document.getElementById("obs");
+    if (started == true && obs.class != "obsMove") obs.classList.add("obsMove");
+}
+
+document.addEventListener("keydown", function (event) {
+    started = true;
+    if (event.key === "Enter" || event.key === " " || event.key === "ArrowUp") {
+        jump();
+        obsMove();
+    }
 });
