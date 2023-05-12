5989a9de30b12c757502a6b2230010fbebbe9d4b
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-05-12 13:13:44 +0400

    added comments

--- backend/login.php
+++ backend/login.php
@@ -1,27 +1,38 @@
 <?php
 
+// getting the database credientials
 $host = "localhost";
 $user = "root";
 $password = "";
 $port = 3306;
 $database = "project";
 
+// connecting to the database
 $conn = new mysqli($host, $user, $password, $database, $port);
 
+// if connection failed
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// getting the email and password
 $email = $_POST["emailadd"];
 $pass = $_POST["pwd"];
 
+// selecting the data
 $cmd = "SELECT * FROM user WHERE email='$email' AND password='$pass'";
 
+// run the query
 $result = $conn->query($cmd);
 
-$conn->close();
-
-header("Location: /index.html");
-exit;
+if ($result->num_rows == 1) {
+    // returning back to the main screen
+    $conn->close();
+    header("Location: /index.html");
+    exit;
+} else {
+    // closing the connection
+    $conn->close();
+}
 
 ?>
--- backend/register.php
+++ backend/register.php
@@ -1,28 +1,36 @@
 <?php
 
+// getting the database credientials
 $host = "localhost";
 $user = "root";
 $password = "";
 $port = 3306;
 $database = "project";
 
+// connecting to the database
 $conn = new mysqli($host, $user, $password, $database, $port);
 
+// if connection failed
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// getting first name, last name, email, and password
 $fname = $_POST["fname"];
 $lname = $_POST["lname"];
 $email = $_POST["emailadd"];
 $pwd = $_POST["pwd"];
 
+// adding the data to the table
 $cmd = "INSERT INTO user (first_name, last_name, password, email) VALUES ('$fname', '$lname', '$pwd', '$email')";
 
+// query it
 $conn->query($cmd);
 
+// close the connection
 $conn->close();
 
+// load to register.html
 header("Location: /register.html");
 exit;
 ?>
